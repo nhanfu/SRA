3c11511facf59b832dfc4da9e1d8a4f3c309a72a
nguyentrantindev@gmail.com <nguyentrantindev@gmail.com>
2023-03-08 14:08:38 +0700

    [add] index2

--- public/form/login/payment_login.meta-v.0.0.1.js
+++ public/form/login/payment_login.meta-v.0.0.1.js
@@ -1,12 +1,20 @@
+import Utils from "/core/utils.js";
 const meta = [
     {
         com: '../core/div/div-v0.0.1.js',
         selector: '#main',
         field: 'main',
         view: '../form/login/index.html',
+        view2: '../form/login/index2.html',
         events: {
             DOMContentLoaded: (args) => {
-                console.log(args);
+                args.ele.querySelector("#submit").addEventListener("click", async (e) => {
+                    e.preventDefault();
+                    args.ele = args.env.querySelector(args.meta.selector);
+                    args.ele.classList.add("user-info");
+                    var template = await Utils.fetchText(args.meta.view2);
+                    args.ele.innerHTML = template;
+                });
             }
         },
         layout: {
